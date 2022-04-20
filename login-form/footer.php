c157790e35e4fbf822de0a206ea52f5677de8f0b
Denis <41701228+danmiles@users.noreply.github.com>
2022-04-20 23:02:07 +0200

    Try to fix login form (with php include header, footer)

---
+++ login-form/footer.php
@@ -0,0 +1,9 @@
+        </div><!-- End container -->
+        
+    </section>
+
+    
+
+    <script src="../assets/js/main.js"></script>
+
+</body>
--- login-form/registration.php
+++ login-form/registration.php
@@ -1,68 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <title>Notice Coding Exercise</title>
-    <meta content="Small exercises while working with the code (web development) that I performed for the Notice company, Paris." name="description">
-    <meta content="web development, code" name="keywords">
-
-    <!-- mobile responsive meta -->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-
-    <!-- Favicon -->
-    <link rel="icon" type="image/png" href="../assets/img/favicon.png">
-
-    <!-- Bootstrap Grid Only -->
-    <link rel="stylesheet" href="../assets/plugins/bootstrap-5.1.3/bootstrap-grid.min.css">
-
-    <!-- Font Awesome Icons -->
-    <link rel="stylesheet" href="../assets/plugins/fontawesome-free-6.1.1-web/css/all.min.css">
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet">
-
-    <!-- Main Stylesheet -->
-    <link rel="stylesheet" href="../assets/css/main.css">
-</head>
-
-<body>
-
-    <!-- ======= Header ======= -->
-    <header id="header" class="header fixed-top">
-        <div class="container-xl d-flex align-items-center justify-content-between">
-
-            <a href="index.html" class="logo d-flex align-items-center">
-                <img src="../assets/img/logo.png" alt="">
-                <span>Notice Coding Exercise </span>
-            </a>
-
-            <nav id="navbar" class="navbar">
-                <ul>
-                    <li><a id="homeActive" class="nav-link scrollto" href="../index.html">Home</a></li>
-                    <li><a class="nav-link scrollto" href="../about.html">About us</a></li>
-                    <li><a class="nav-link scrollto" href="../contact.html">Contact</a></li>
-                    <li><a class="nav-link scrollto" href="../profile.html">Profile</a></li>
-                    <li><a class="nav-link scrollto" href="../time.html">Get Time</a></li>
-                    <li>
-                        <a class="nav-link scrollto " href="index.php"><i class="fa-solid fa-user-shield"></i>Log In</a>
-
-                        <a class="nav-link scrollto active" href="registration.php"><i class="fa-solid fa-user-plus"></i>Sign Up</a>
-                    </li>
-                </ul>
-                <i class="bx bx-list-plus mobile-nav-toggle"></i>
-            </nav><!-- .navbar -->
-
-        </div>
-    </header><!-- End Header -->
-
-    <section class="registration-form">
-
-        <div class="container-xxl">
-
+<?php include 'header.php' ?>
             <?php
             // Start session 
             session_start();
@@ -102,10 +38,4 @@
                 </form>
             </div>
 
-        </div> <!-- End Container -->
-
-    </section>
-
-    <script src="../assets/js/main.js"></script>
-
-</body>
+<?php include 'footer.php' ?>
